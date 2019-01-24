048242910d71309e84bd3731759801241d6bed5f
алый <iseurie@gmail.com>
2019-01-24 14:17:40 -0500

    exploit commutativity: eliminate redundant combinations in inner loop

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,14 +1,11 @@
 #include "max_product.h"
 
-int max_pairwise_product_naive(int * arr, int size) {
+int max_pairwise_product_fast(int * arr, int size) {
     int max = arr[0] * arr[1];
     for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (i != j) {
-                int p = arr[i] * arr[j];
-                if (p > max) {
-                    max = p;
-                }
+        for (int j = i+1; j < size; j++) {
+            if (p > max) {
+                max = p;
             }
         }
     }
