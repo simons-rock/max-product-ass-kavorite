aac70f5fe6b0be90b2b8d2878f0fcfc4bfe24d1e
алый <iseurie@gmail.com>
2019-01-24 14:16:49 -0500

    naive implementation

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,16 @@
 #include "max_product.h"
 
-int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+int max_pairwise_product_naive(int * arr, int size) {
+    int max = arr[0] * arr[1];
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i != j) {
+                int p = arr[i] * arr[j];
+                if (p > max) {
+                    max = p;
+                }
+            }
+        }
+    }
+    return max;
 }
-
