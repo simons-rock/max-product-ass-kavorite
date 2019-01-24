b000d303eeefe9b2e104c3a42622f1efe496eb26
алый <iseurie@gmail.com>
2019-01-24 15:20:57 -0500

    rm redundant product computation

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,9 +1,10 @@
 #include "max_product.h"
 
 int max_pairwise_product_fast(int * arr, int size) {
-    int max = arr[0] * arr[1];
+    int max = 0;
     for (int i = 0; i < size; i++) {
         for (int j = i+1; j < size; j++) {
+            int p = arr[i] * arr[j];
             if (p > max) {
                 max = p;
             }
