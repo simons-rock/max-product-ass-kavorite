75ca9a4e719cc2e96aa8a3db2c61d528785cc970
алый <iseurie@gmail.com>
2019-01-29 12:59:43 -0500

    reimplement with pairwise linear search

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,14 +1,18 @@
 #include "max_product.h"
 
 int max_pairwise_product_fast(int * arr, int size) {
-    int max = 0;
-    for (int i = 0; i < size; i++) {
-        for (int j = i+1; j < size; j++) {
-            int p = arr[i] * arr[j];
-            if (p > max) {
-                max = p;
-            }
+    int i_max = 0;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > arr[i_max]) {
+            i_max = i;
         }
     }
-    return max;
+    int i_sndmax = 0;
+    for (int i = 0; i < size; i++) {
+        if (i_sndmax == i_max) continue;
+        if (arr[i] > arr[i_sndmax]) {
+            i_sndmax = i;
+        }
+    }
+    return arr[i_max * i_sndmax];
 }
